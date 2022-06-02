bf051c3b17bfc6719b59a1896e0b02ce10429db6
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-02 20:13:01 +0100

    Task 8 updated 0;

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 65, 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", (1+60), 1, stderr);
 	return (1);
 }

8e75984fef4bec41155b3eeae173626efc9a9fe9
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-02 15:09:07 +0100

    Finished the tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+/*
+ * main.c
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

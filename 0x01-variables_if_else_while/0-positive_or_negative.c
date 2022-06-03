4128dc9884ece8a92295978e409643db126b35f0
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-03 10:00:41 +0100

    Task 0 started

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints out the sign value of a random number
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive");
+	} else if (n == 0)
+	{
+		printf("is zero");
+	} else 
+	{
+		printf("is negative");
+	}
+	return (0);
+}

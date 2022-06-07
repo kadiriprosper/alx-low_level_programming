814acdfbfe79965e56bd50abc01516a0325f3a26
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 20:42:14 +0100

    sss

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,15 @@
 
 void print_alphabet_x10(void)
 {
-int a = 97;
-int n;
-for(n = 0; n < 10; n++)
+int a = 'a';
+int n = 0;
+while (n < 10)
 {
-    while (a < 123)
+    while (a < 'z')
     {
        _putchar(a);
         a++;
     }
+n++;
 }
 }

47d28941531445c874e12f22930774437fd1801c
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 20:21:18 +0100

    dddd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,15 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - prints out the alphabet
+ * 
+ * 
+ */
+void print_alphabet(void)
+{
+    int a = 97;
+    while (a < 122)
+    {
+        _putchar(a + '0');
+        _putchar('\n');
+    }
+}

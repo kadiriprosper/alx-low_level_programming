ae3265a8b99eaeeef6add4099b07db76a2788949
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 20:51:19 +0100

    sss

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
  * main - prints out the alphabet
- * 
- * @param c 
+ *
+ * @param c
  * Return: 0
  */
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
-    return (1);
+{
+return (1);
+}
 else
-    return (0);
+{
+return (0);
+}
 }

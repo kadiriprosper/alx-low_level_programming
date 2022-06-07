aadeeb3d1b05c56b76c93441fe48c80bad1fb97b
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 20:07:26 +0100

    Revamped main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,11 @@ void _putchar(void)
 printf("_putchar\n");
 }
 
-void print_alphabet(void);
+void print_alphabet(void)
+{
+    int a = 97;
+    while (a < 122)
+    {
+        putchar(a + '0');
+    }
+}

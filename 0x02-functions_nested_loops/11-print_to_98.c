9ac3a7d1d76b68feda5d42592e7c229b5d114dcc
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 22:45:19 +0100

    dfdfd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,28 @@ if (n < 98)
 {
 for (i = n; i <= 98; i++)
 {
-printf("%d, ", i);
+if(i == 98)
+{
+    printf("%d\n", i);
+}
+else
+{
+    printf("%d, ", i);
+}
 }
 }
 else
 {
 for (i = n; i >= 98; i--)
 {
-printf("%d, ", i);
+if(i == 98)
+{
+    printf("%d\n", i);
+}
+else
+{
+    printf("%d, ", i);
+}
 }
 }
 printf("\n");

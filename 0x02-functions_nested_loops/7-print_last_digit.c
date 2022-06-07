69945be6833aaf24c97f8348119b9d09b47a4415
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 21:16:10 +0100

    ddddddd

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints out the absolute value of a number
+ *
+ * @c: the int to be worked with
+ * Return: 0
+ */
+
+int print_last_digit(int c)
+{
+return (c % 0);
+}

9792e92a71b04d84354236d3318d52b8399ba491
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 20:08:27 +0100

    Revamped alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include "main.h"
-void print_alphabet(void)
+
+int main (void)
 {
-    int a = 97;
-    while (a < 122)
-    {
-        putchar(a + '0');
-    }
+    print_alphabet();
+    return (0);
 }

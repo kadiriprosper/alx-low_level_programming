893ce8cea39462c283185b52cc5fbc1ed0dbcbcd
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 20:14:54 +0100

    aaa

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,12 @@ void _putchar(char a)
 putchar(a);
 }
 
-void print_alphabet(void);
+void print_alphabet(void)
+{
+    int a = 97;
+    while (a < 122)
+    {
+        _putchar(a + '0');
+        _putchar('\n');
+    }
+}

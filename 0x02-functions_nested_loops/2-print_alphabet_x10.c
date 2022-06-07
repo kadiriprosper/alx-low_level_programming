95026381de8cfc55eb8866ec9cb7029d2f810577
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 20:37:39 +0100

    dddd

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints out the alphabet
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+int a = 97;
+int n = 0;
+while (n < 10)
+{
+while (a < 123)
+{
+    _putchar(a);
+    a++;
+}
+n++;
+}
+}

9cf1cecd56253ff305287d204058f8ff588ba877
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 20:29:09 +0100

    ddd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  */
 void print_alphabet(void)
 {
-    char i;
+char i;
 
-    for (i = 'a'; i <= 'z'; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
+for (i = 'a'; i <= 'z'; i++)
+{
+    _putchar(i);
+}
+_putchar('\n');
 }

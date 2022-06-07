08de97182a87e09dab1109b2423a2da5c3f8cdde
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 21:50:00 +0100

    sdferf

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - a function that returns the sum of two integers
+ *
+ * @a - param a
+ * @b - param b
+ *
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}

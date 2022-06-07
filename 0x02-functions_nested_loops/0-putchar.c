fe5669ca2167311d090c52b6f1dcbf3b623b3897
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-07 20:34:32 +0100

    ddd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,12 +3,12 @@
 
 /**
  * main - prints putchar to the console
- * 
+ *
  * Return: 0
  *
  */
 int main(void)
 {
-    printf("_putchar\n");
-    return (0);
+printf("_putchar\n");
+return (0);
 }

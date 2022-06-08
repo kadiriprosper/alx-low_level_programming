83b0bc8eb42c83b6dbff5021b970d90d94b02b6a
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-08 02:24:58 +0100

    trial 3

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,30 +2,32 @@
 
 /**
  * main - prints the first 50 fibonacci starting with 1 and 2
- * 
+ *
  * Return: 0
  */
 
 int main(void)
 {
-    long a = 1l;
-    long b = 2l;
-    long sum = 0l;
-    long n = 0l;
-    printf("%ld, ", a);
-    printf("%ld, ", b);
-    while (n < 50)
-    {
-        sum = a + b;
-        a = b;
-        b = sum;
-        if (n < 49)
-        {
-            printf("%ld, ", sum);
-        }else{
-            printf("%ld\n", sum);
-        }
-        n++;
-    }
-    return (0);
+long a = 1l;
+long b = 2l;
+long sum = 0l;
+long n = 0l;
+printf("%ld, ", a);
+printf("%ld, ", b);
+while (n < 48)
+{
+sum = a + b;
+a = b;
+b = sum;
+if (n < 47)
+{
+printf("%ld, ", sum);
+}
+else
+{
+printf("%ld\n", sum);
+}
+n++;
+}
+return (0);
 }

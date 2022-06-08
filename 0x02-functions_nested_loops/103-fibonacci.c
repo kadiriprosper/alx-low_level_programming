062f42b504919e63513db5e1d098a9a53aea1d7b
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-08 02:28:33 +0100

    103 trial 1

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci starting with 1 and 2
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+long a = 1l;
+long b = 2l;
+long sum = 0l;
+long n = 0l;
+printf("%ld, ", a);
+printf("%ld, ", b);
+while (n < 48)
+{
+sum = a + b;
+a = b;
+b = sum;
+if ((sum <= 4000000) && (sum % 2 == 0))
+{
+    printf("%ld", sum);
+}
+n++;
+}
+return (0);
+}

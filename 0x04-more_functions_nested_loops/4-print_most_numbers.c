017684e5d47636fda2f13062a07baa78eb85bfb0
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-09 21:12:00 +0100

    sdsd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_most_numbers - prints the numbers, from 0 to 9, except 2 and 4
  *
@@ -13,9 +14,9 @@ while (i <= '9')
 {
 if (i != '2' && i != '4')
 {
-_putchar(i);
+printf("%d", i);
 }
 i++;
 }
-_putchar('\n');
+printf("\n");
 }

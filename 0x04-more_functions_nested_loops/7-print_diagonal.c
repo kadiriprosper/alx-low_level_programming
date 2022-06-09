3c3e63246e9308754cc71f96ac6f504dce55fae5
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-09 21:48:15 +0100

    sdfd

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal of n characters
+ *
+ * @n: Param length
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+int i, j;
+
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+    _putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}

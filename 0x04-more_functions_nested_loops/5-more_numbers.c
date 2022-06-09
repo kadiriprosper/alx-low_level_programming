dcfdf72157c711fbe219a7ff6b0e3053889cbae7
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-09 21:33:33 +0100

    sdf

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
@@ -8,10 +9,15 @@
 void print_numbers(void)
 {
 int i;
+int j = 0;
 
-for (i = '0'; i <= '9'; i++)
+while (j <= 10)
+{
+for (i = 0; i <= 14; i++)
 {
 printf("%d", i);
 }
 printf("\n");
+j++;
+}
 }

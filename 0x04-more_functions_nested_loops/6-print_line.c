16463c717d9be936676af81d95545d04d64ff98c
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-09 21:37:50 +0100

    wdfghj

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - print a line of n characters
+ * 
+ * Return: n
+ */
+
+void print_line(int n)
+{
+    int i;
+
+    for (i = 0; i < n; i++)
+    {
+        _putchar('_');
+    }
+    _putchar('\n');
+}

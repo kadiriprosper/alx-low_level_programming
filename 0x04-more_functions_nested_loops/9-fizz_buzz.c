783d10c6375de5881852feb90424a055e2b58997
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-09 22:03:22 +0100

    dfgff

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - prints out fizz buzz
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int i = 0;
+while (i <= 100)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf("Buzz");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf('%d', i);
+}
+}
+return (0);
+}

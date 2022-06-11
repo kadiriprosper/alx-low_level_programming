df0cacdd7ca913a5aece5560ac89eebd45b392a7
kadiriprosper <34899972+kadiriprosper@users.noreply.github.com>
2022-06-11 04:21:30 +0100

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,30 +8,23 @@
  */
 void print_triangle(int size)
 {
-int i;
-int j;
-int k;
+	int i = 0, j, n = size - 1;
 
-if (size <= 0)
-{
-_putchar('\n');
-}
-else
-{
-for (i = 1; i <= size; i++)
-{
-for (j = 1; j <= size; j++)
-{
-if (j <= size - i)
-{
-_putchar(' ');
-}
-else
-{
-_putchar('#');
-}
-}
-_putchar('\n');
-}
-}
+	if (size > 0)
+	{
+		for (; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
 }

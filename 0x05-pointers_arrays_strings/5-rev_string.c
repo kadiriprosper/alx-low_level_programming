caebf84f17ac5c3479e83cc9cb5053ada43e5247
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-14 00:21:59 +0100

    cv

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
  */
 void rev_string(char *s)
 {
-char a = *s;
+char *a = s;
 int p = 0;
 int length = 0;
 while (s[length] != '\0')
@@ -21,5 +21,4 @@ a[p] = s[length];
 length--;
 p++;
 }
-s = &a;
 }

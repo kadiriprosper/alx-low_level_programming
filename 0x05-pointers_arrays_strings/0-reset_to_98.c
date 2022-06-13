e7f27638e35ef5bcbbbaa7a3b02b4b921b3eb87f
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-13 14:20:28 +0100

    gtg

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 
 /**
  * reset_to_98 - changes the value of a variable to 98
- * @*n: pointer to the variable to change
+ * @n: pointer to the variable to change
  * Return: 0
  */
 int reset_to_98(int *n)

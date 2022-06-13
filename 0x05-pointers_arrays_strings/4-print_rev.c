c4c5f07ab0d6c8a9d98d1bf8a06ea18786a43ed9
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-13 18:02:51 +0100

    fff

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string to the standard output in reverse.
+ * @s: string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i = 0;
+int len = _strlen(s);
+
+while (i < len)
+{
+putchar(s[len - 1]);
+len--;
+}
+}

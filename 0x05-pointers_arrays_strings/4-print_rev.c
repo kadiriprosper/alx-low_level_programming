350a979adaf4eec7cf7b2cbb561166b6ce381a2b
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-14 01:53:10 +0100

    cddc

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,16 +11,19 @@ void print_rev(char *s)
 int i = 1;
 int j = 0;
 int len = 0;
+int p = 0;
+
 while (s[len] != '\0')
 {
 len++;
 }
+p = len;
 while (i < len)
 {
 putchar(s[len - 1]);
 len--;
 }
-if(len > 1)
+if(p > 1)
 {
 putchar(s[j]);
 }

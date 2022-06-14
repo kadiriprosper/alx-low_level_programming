292ec3bff4f49bceaeb883b15793890e6910a23b
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-14 01:07:14 +0100

    sds

---
+++ 0x05-pointers_arrays_strings/6-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_Array - prints an array of integers
+ * @a: array of integers
+ * Return: void
+ */
+void print_Array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i != n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}

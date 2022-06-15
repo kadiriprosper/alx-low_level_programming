ea375b3a5d1ea56647bbd91eb56df49447461cbe
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-15 18:31:25 +0100

    sde

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,14 @@ i = 0;
 while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] > s2[i])
-return (1);
+return (15);
 if (s1[i] < s2[i])
-return (-1);
+return (-15);
 i++;
 }
 if (s1[i] == '\0' && s2[i] != '\0')
-return (-1);
+return (-15);
 if (s1[i] != '\0' && s2[i] == '\0')
-return (1);
+return (15);
 return (0);
 }

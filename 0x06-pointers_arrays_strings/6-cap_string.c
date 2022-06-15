61cb7d8e6a17a20bd821f6cc6dff8a052bee7a84
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-15 18:44:54 +0100

    dedd

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_stirng - capitalizes all words of a string
+ * @str: string to capitalize
+ * Return: pointer to str
+ */
+char *cap_string(char *str)
+{
+int i, j;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ' || str[i] == '\t')
+{
+for (j = i + 1; str[j] != '\0'; j++)
+{
+if (str[j] >= 'a' && str[j] <= 'z')
+str[j] = str[j] - 32;
+}
+}
+}
+return (str);
+}

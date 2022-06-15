f253d4ef271c058f375114312eddf6c46fff6fb5
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-15 18:30:26 +0100

    ffrr

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 0 if equal, 1 if s1 > s2, -1 if s1 < s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+
+i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] > s2[i])
+return (1);
+if (s1[i] < s2[i])
+return (-1);
+i++;
+}
+if (s1[i] == '\0' && s2[i] != '\0')
+return (-1);
+if (s1[i] != '\0' && s2[i] == '\0')
+return (1);
+return (0);
+}

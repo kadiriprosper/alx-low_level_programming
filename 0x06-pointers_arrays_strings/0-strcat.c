fbde527c8ed89ca3f9affc43f6875dd8e65266c0
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-15 18:18:12 +0100

    frfrr

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * strcat - concatenates two strings
- * @dest: destination string
- * @src: source string
+ * *_strcat - concatenates two strings
+ * @*dest: destination string
+ * @*src: source string
  * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)

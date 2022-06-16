37627c7206e5e7de64e2f993ba98a9b7bd5b9311
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-16 22:59:33 +0100

    stuff changed in 3

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,14 @@
  */
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n *= -1;
-    }
-    if (n / 10 != 0)
-    {
-        _putchar(n / 10);
-    }
-    _putchar(n % 10 + '0');
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+if (n / 10 != 0)
+{
+_putchar(n / 10);
+}
+_putchar(n % 10 + '0');
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,13 +13,13 @@ int i;
 for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
 if (s1[i] > s2[i])
-return (15);
+return (1);
 if (s1[i] < s2[i])
-return (-15);
+return (-1);
 }
 if (s1[i] == '\0' && s2[i] != '\0')
-return (-15);
+return (-1);
 if (s1[i] != '\0' && s2[i] == '\0')
-return (15);
+return (1);
 return (0);
 }

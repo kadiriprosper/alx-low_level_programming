cf75a08fa8e01ac109518614ad951b515183d513
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-16 18:53:52 +0100

    llk

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_number - prints a number
@@ -8,14 +7,14 @@
  */
 void print_number(int n)
 {
-if (n < 0)
-{
-putchar('-');
-n *= -1;
-}
-if (n / 10 != 0)
-{
-print_number(n / 10);
-}
-putchar(n % 10 + '0');
+    if (n < 0)
+    {
+        _putchar('-');
+        n *= -1;
+    }
+    if (n / 10 != 0)
+    {
+        _putchar(n / 10);
+    }
+    _putchar(n % 10 + '0');
 }

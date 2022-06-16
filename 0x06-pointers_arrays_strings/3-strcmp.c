f0ee3a886f0924b0af1a9e3139a5a8b5bdc531e4
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-16 18:28:53 +0100

    ddr

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,12 @@ int _strcmp(char *s1, char *s2)
 {
 int i;
 
-i = 0;
-while (s1[i] != '\0' && s2[i] != '\0')
+for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
 if (s1[i] > s2[i])
 return (1);
 if (s1[i] < s2[i])
 return (-1);
-i++;
 }
 if (s1[i] == '\0' && s2[i] != '\0')
 return (-1);

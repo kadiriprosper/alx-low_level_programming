8e9c4dd22e71fc89d3c997797ec2a7061804b74b
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-16 18:52:00 +0100

    sed

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - prints a number
@@ -9,12 +10,12 @@ void print_number(int n)
 {
 if (n < 0)
 {
-_putchar('-');
+putchar('-');
 n *= -1;
 }
 if (n / 10 != 0)
 {
 print_number(n / 10);
 }
-_putchar(n % 10 + '0');
+putchar(n % 10 + '0');
 }

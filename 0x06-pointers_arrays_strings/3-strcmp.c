a51a18ba0214ff1546b19427f5214b0226603fff
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-16 23:01:50 +0100

    df

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,13 +13,21 @@ int i;
 for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
 if (s1[i] > s2[i])
+{
 return (1);
+}
 if (s1[i] < s2[i])
+{
 return (-1);
 }
+}
 if (s1[i] == '\0' && s2[i] != '\0')
+{
 return (-1);
+}
 if (s1[i] != '\0' && s2[i] == '\0')
+{
 return (1);
+}
 return (0);
 }

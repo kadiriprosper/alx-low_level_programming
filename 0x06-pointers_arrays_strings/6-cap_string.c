788cf525df357e828e304bde6f0365dabaaa18a3
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-16 18:20:09 +0100

    sded

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,19 +7,4 @@
  */
 char *cap_stirng(char *str)
 {
-int i, j;
-char *aux;
-
-aux = str;
-for (i = 0; str[i]; i++)
-{
-if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
-{
-for (j = i + 1; str[j] && str[j] != ' ' && str[j] != '\t' && str[j] != '\n'; j++)
-{
-str[j] = toupper(str[j]);
-}
-}
-}
-return (aux);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,3 +5,22 @@
  * @str: string to encode
  * Return: pointer to str
  */
+char *leet(char *str)
+{
+int i, j;
+char *leet_str = str;
+char *leet_table = "a4e3g9i8o0s5t7z";
+char *leet_table_2 = "A4E3G9I8O0S5T7Z";
+
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; leet_table[j] != '\0'; j++)
+{
+if (str[i] == leet_table[j])
+{
+leet_str[i] = leet_table_2[j];
+}
+}
+}
+return (leet_str);
+}

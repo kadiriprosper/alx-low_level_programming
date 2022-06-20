6ef4daf76b40231fa32a9f107c3f00d9c207bc6a
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-21 00:32:39 +0100

    ss

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,11 @@ char *_strchr(char *s, char c)
 int i = 0;
 while (s[i] != '\0')
 {
-    if (s[i] == c)
-    {
-        return (&s[i]);
-    }
-    i++;
+if (s[i] == c)
+{
+return (&s[i]);
+}
+i++;
 }
 return ('NULL');
 }

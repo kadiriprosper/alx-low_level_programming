48eb1a0f74e47f21c9683bc356f3fdc3c40f8f78
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-21 00:58:53 +0100

    lll

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints a chessboard
+ * @a: pointer to the first row of the board
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+if (j == 0)
+printf("%c", a[i][j]);
+else
+printf(" %c", a[i][j]);
+}
+printf("\n");
+}
+}

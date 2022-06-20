57327ad2103db31e31dad32f92a18598dbafd75e
Kadiri Ehijie <kadiriehijie@gmail.com>
2022-06-21 00:25:14 +0100

    removed trailing spaces

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - locates a character in a string
  * @s: The string to search
  * @c: The character to search for
- * Return: pointer to the first occurrence of c in s    
+ * Return: pointer to the first occurrence of c in s
  */
 
 char *_strchr(char *s, char c)

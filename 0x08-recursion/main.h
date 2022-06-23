5121dfc4581d910a23a423cb4ceb6bef01b701d6
kadiriprosper <34899972+kadiriprosper@users.noreply.github.com>
2022-06-23 12:32:36 +0100

    gg

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,6 @@ int factorial(int n);
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
+int _squareRoot(int n, int i);
+int divisors(int n, int m);
+int tmp_prime(int n, int i);

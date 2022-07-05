8f591c6d13e2dde37dbd230e3beab3ca3e4e1b62
kadiriprosper <34899972+kadiriprosper@users.noreply.github.com>
2022-07-05 01:37:31 +0000

    onStruct

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("%s\n", _FILE_);
+return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef MACRO
+#define MACRO
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
+#endif

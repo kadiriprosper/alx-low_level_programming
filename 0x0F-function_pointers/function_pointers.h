c19bcf2953df984cce105c43abfedc30ad16c871
kadiriprosper <34899972+kadiriprosper@users.noreply.github.com>
2022-07-07 22:33:37 +0000

    included guard

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef function_pointers
-#define function_pointers
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));

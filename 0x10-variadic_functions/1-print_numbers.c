201c1c3377d2365640a37f6e9ec3e5cec0f47330
kadiriprosper <34899972+kadiriprosper@users.noreply.github.com>
2022-07-07 22:46:54 +0000

    message

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+int number = 0;
+
+va_list ptr;
+
+va_start(ptr, n);
+
+for(int a = 0; a < n; a++)
+{
+if (*separator)
+printf("%d%c ", va_arg(ptr, int), *separator);
+}
+printf("\n");
+}
